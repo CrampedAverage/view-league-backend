a36d2f1048244ec6044e896f998d023df19da0ba
CrampedAverage <adnan.aant@gmail.com>
2022-10-22 20:36:27 +0100

    Added dotenv to be able to read environment variables

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,40 @@ async fn champions() -> ChampionsResult {
     }
 }
 
+#[get("/summoner/{name}")]
+async fn player(config: web::Data<Configuration>) -> Responder {
+    let found = false;
+    println!("Adnan: {}", config.riot_api_key);
+    if found {
+        Either::Left(HttpResponse::Ok().body("FOUND"))
+    } else {
+        Either::Right(HttpResponse::BadRequest().body("Not FOUND"))
+    }
+}
+#[derive(Deserialize, Debug, Clone)]
+struct Configuration {
+    port: u16,
+    riot_api_key: String,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv::dotenv().expect("Failed to read .env file");
+    match envy::from_env::<Configuration>() {
+        Ok(config) => println!("{:?}", config),
+        Err(e) => println!("Couldn't read config ({})", e),
+    };
+    let config = envy::from_env::<Configuration>().unwrap();
+    let port = config.port;
+    // env::var(key)
+    HttpServer::new(move || {
         App::new()
+            .app_data(config.clone())
             .wrap(middleware::Compress::default())
             .service(champions)
+        // .service(player)
     })
-    .bind(("127.0.0.1", 4040))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }
