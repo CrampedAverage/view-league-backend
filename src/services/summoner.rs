c1189b8f5f1db2b16432a650cb8c873704109cf8
CrampedAverage <adnan.aant@gmail.com>
2022-10-24 15:22:43 +0100

    Added a new function to get summoner ranks

--- src/services/summoner.rs
+++ src/services/summoner.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[allow(non_snake_case)]
@@ -13,8 +15,8 @@ pub struct SummonerInfo {
 }
 
 pub async fn get_summoner_info(
-    region: String,
-    name: String,
+    region: &String,
+    name: &String,
     api_key: &String,
 ) -> Result<SummonerInfo, reqwest::Error> {
     let url = format!(
@@ -25,15 +27,38 @@ pub async fn get_summoner_info(
     Ok(response)
 }
 
+async fn get_summoner_ranks(
+    region: &String,
+    name: &String,
+    api_key: &String,
+) -> Result<HashMap<String, String>, reqwest::Error> {
+    let url = format!(
+        "https://{0}.api.riotgames.com/lol/league/v4/entries/by-summoner/{1}?api_key={2}",
+        region, name, api_key
+    );
+    let response = reqwest::get(url)
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+
+    Ok(response)
+}
+
 pub async fn get_summoner_data(
     region: String,
     name: String,
     api_key: &String,
 ) -> Result<SummonerInfo, String> {
-    let summoner_info_result = get_summoner_info(region, name, api_key).await;
+    let summoner_info_result = get_summoner_info(&region, &name, api_key).await;
     if summoner_info_result.is_err() {
         let error_response = format!("{}", summoner_info_result.unwrap_err());
         return Err(error_response);
     }
+    let summoner_ranks_result = get_summoner_ranks(&region, &name, api_key).await;
+    if summoner_ranks_result.is_err() {
+        let error_response = format!("{}", summoner_ranks_result.unwrap_err());
+        return Err(error_response);
+    }
+    if summoner_info_result.is_err() {}
     return Ok(summoner_info_result.unwrap());
 }
