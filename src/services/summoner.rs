88eeb099c9fab0dfc3aecc02f8148bd89bfd359d
CrampedAverage <adnan.aant@gmail.com>
2022-10-24 14:25:42 +0100

    Added new service function for getting summoners information

---
+++ src/services/summoner.rs
@@ -0,0 +1,39 @@
+use serde::{Deserialize, Serialize};
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SummonerInfo {
+    id: String,
+    accountId: String,
+    puuid: String,
+    name: String,
+    profileIconId: u32,
+    revisionDate: u64,
+    summonerLevel: u32,
+}
+
+pub async fn get_summoner_info(
+    region: String,
+    name: String,
+    api_key: &String,
+) -> Result<SummonerInfo, reqwest::Error> {
+    let url = format!(
+        "https://{0}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{1}?api_key={2}",
+        region, name, api_key
+    );
+    let response = reqwest::get(url).await?.json::<SummonerInfo>().await?;
+    Ok(response)
+}
+
+pub async fn get_summoner_data(
+    region: String,
+    name: String,
+    api_key: &String,
+) -> Result<SummonerInfo, String> {
+    let player_info_result = get_summoner_info(region, name, api_key).await;
+    if player_info_result.is_err() {
+        let error_response = format!("{}", player_info_result.unwrap_err());
+        return Err(error_response);
+    }
+    return Ok(player_info_result.unwrap());
+}
