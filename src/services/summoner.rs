5204ca50234435ea54a9629300695ed299488ee8
CrampedAverage <adnan.aant@gmail.com>
2022-10-24 15:37:23 +0100

    Renamed structs and created new struct for summoner ranks api response

--- src/services/summoner.rs
+++ src/services/summoner.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 
 #[allow(non_snake_case)]
 #[derive(Serialize, Deserialize, Debug)]
-pub struct SummonerInfo {
+pub struct SummonerInfoResponse {
     id: String,
     accountId: String,
     puuid: String,
@@ -18,27 +18,42 @@ pub async fn get_summoner_info(
     region: &String,
     name: &String,
     api_key: &String,
-) -> Result<SummonerInfo, reqwest::Error> {
+) -> Result<SummonerInfoResponse, reqwest::Error> {
     let url = format!(
         "https://{0}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{1}?api_key={2}",
         region, name, api_key
     );
-    let response = reqwest::get(url).await?.json::<SummonerInfo>().await?;
+    let response = reqwest::get(url)
+        .await?
+        .json::<SummonerInfoResponse>()
+        .await?;
     Ok(response)
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct StatusBody {
+    message: String,
+    status_code: u32,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct SummonerRanksResponse {
+    status: StatusBody,
 }
 
 async fn get_summoner_ranks(
     region: &String,
     name: &String,
     api_key: &String,
-) -> Result<HashMap<String, String>, reqwest::Error> {
+) -> Result<SummonerRanksResponse, reqwest::Error> {
     let url = format!(
         "https://{0}.api.riotgames.com/lol/league/v4/entries/by-summoner/{1}?api_key={2}",
         region, name, api_key
     );
+    println!("{}", url);
     let response = reqwest::get(url)
         .await?
-        .json::<HashMap<String, String>>()
+        .json::<SummonerRanksResponse>()
         .await?;
 
     Ok(response)
@@ -48,7 +63,7 @@ pub async fn get_summoner_data(
     region: String,
     name: String,
     api_key: &String,
-) -> Result<SummonerInfo, String> {
+) -> Result<SummonerInfoResponse, String> {
     let summoner_info_result = get_summoner_info(&region, &name, api_key).await;
     if summoner_info_result.is_err() {
         let error_response = format!("{}", summoner_info_result.unwrap_err());
@@ -59,6 +74,6 @@ pub async fn get_summoner_data(
         let error_response = format!("{}", summoner_ranks_result.unwrap_err());
         return Err(error_response);
     }
-    if summoner_info_result.is_err() {}
+    println!("{:#?}", summoner_ranks_result.unwrap());
     return Ok(summoner_info_result.unwrap());
 }
