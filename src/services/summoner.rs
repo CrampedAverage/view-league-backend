b917546da5906c5f82cec2350b2ca89bf0a8a2bc
CrampedAverage <adnan.aant@gmail.com>
2022-10-25 02:06:42 +0100

    Completed the function fetching the summoner's rank

--- src/services/summoner.rs
+++ src/services/summoner.rs
@@ -1,4 +1,8 @@
 use serde::{Deserialize, Serialize};
+use std::{
+    error,
+    fmt::{self},
+};
 
 #[allow(non_snake_case)]
 #[derive(Serialize, Deserialize, Debug)]
@@ -31,28 +35,65 @@ pub async fn get_summoner_info(
 #[derive(Deserialize, Serialize, Debug)]
 struct StatusBody {
     message: String,
-    status_code: u32,
+    status_code: u16,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
-struct SummonerRanksResponse {
+struct Status {
     status: StatusBody,
+}
+
+// struct SummonerRanks {}
+// type StringOrInteger = String | U32;
+#[allow(non_snake_case)]
+#[derive(Deserialize, Serialize, Debug)]
+struct SummonerRankInfo {
+    leagueId: String,
+    queueType: String,
+    tier: String,
+    rank: String,
+    summonerId: String,
+    summonerName: String,
+    leaguePoints: u8,
+    wins: u32,
+    losses: u32,
+    veteran: bool,
+    inactive: bool,
+    freshBlood: bool,
+    hotStreak: bool,
+}
+
+#[derive(Debug, Clone)]
+struct GetSummonerRanksError {}
+
+impl fmt::Display for GetSummonerRanksError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "invalid something broke")
+    }
+}
+
+type FetchResult<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(untagged)]
+enum SummonerRanksResponse {
+    Ranks(Vec<SummonerRankInfo>),
+    Error(Status),
 }
 
 async fn get_summoner_ranks(
     region: &String,
-    name: &String,
+    id: &String,
     api_key: &String,
-) -> Result<SummonerRanksResponse, reqwest::Error> {
+) -> FetchResult<SummonerRanksResponse> {
     let url = format!(
         "https://{0}.api.riotgames.com/lol/league/v4/entries/by-summoner/{1}?api_key={2}",
-        region, name, api_key
+        region, id, api_key
     );
     println!("{}", url);
-    let response = reqwest::get(url)
-        .await?
-        .json::<SummonerRanksResponse>()
-        .await?;
+    let raw_response = reqwest::get(url).await?.text().await?;
+    let response = serde_json::from_str::<SummonerRanksResponse>(&raw_response)?;
+    // println!("Hey Adnan: {:#?}", response);
 
     Ok(response)
 }
@@ -67,11 +108,14 @@ pub async fn get_summoner_data(
         let error_response = format!("{}", summoner_info_result.unwrap_err());
         return Err(error_response);
     }
-    let summoner_ranks_result = get_summoner_ranks(&region, &name, api_key).await;
+    let summoner_info = summoner_info_result.unwrap();
+    let summoner_ranks_result = get_summoner_ranks(&region, &summoner_info.id, api_key).await;
     if summoner_ranks_result.is_err() {
-        let error_response = format!("{}", summoner_ranks_result.unwrap_err());
+        let error_response = format!("Error: {}", summoner_ranks_result.unwrap_err());
         return Err(error_response);
     }
-    println!("{:#?}", summoner_ranks_result.unwrap());
-    return Ok(summoner_info_result.unwrap());
+    let summoner_ranks = summoner_ranks_result.unwrap();
+    // println!("{:#?}", summoner_info);
+    // println!("Hey Adnan:{:#?}", summoner_ranks_result.unwrap());
+    return Ok(summoner_info);
 }
