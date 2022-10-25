28c6f71238ac066ac2f63b68731ac5831259379f
CrampedAverage <adnan.aant@gmail.com>
2022-10-26 00:52:51 +0100

    Imported all the files used in main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use crate::services::{champions::get_champions, summoner::get_summoner_data};
-use actix_web::{
-    get, middleware,
-    web::{self, Data},
-    App, Either, HttpResponse, HttpServer,
+use crate::{
+    configuration::get_api_key,
+    services::{champions::get_champions, summoner::get_summoner_data},
 };
+use actix_web::{get, middleware, web, App, Either, HttpResponse, HttpServer};
 use serde::Deserialize;
+
+mod configuration;
+mod lib;
 mod services;
+mod types;
 
 type Responder = Either<HttpResponse, HttpResponse>;
 #[get("/champions")]
@@ -20,51 +23,35 @@ async fn champions() -> Responder {
 }
 
 #[derive(Deserialize, Debug)]
-struct SummonerGetDataQuery {
+pub struct SummonerGetDataQuery {
     summoner_name: String,
     region: String,
     continent: String,
 }
 
 #[get("/summoner/get-data")]
-async fn summoner(
-    query: web::Query<SummonerGetDataQuery>,
-    config: web::Data<Configuration>,
-) -> Responder {
+async fn summoner(query: web::Query<SummonerGetDataQuery>) -> Responder {
     let found = false;
-    let api_key = &config.riot_api_key;
+    let api_key = get_api_key();
+    println!("hehe");
+    let result = get_summoner_data(query.into_inner(), &api_key).await;
 
-    // let result = get_summoner_data(region, name, api_key).await;
-    // println!("{:#?}", result.unwrap());
     if found {
         Either::Left(HttpResponse::Ok().body("FOUND"))
     } else {
         Either::Right(HttpResponse::BadRequest().body("Not FOUND"))
     }
 }
-#[derive(Deserialize, Debug, Clone)]
-struct Configuration {
-    port: u16,
-    riot_api_key: String,
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().expect("Failed to read .env file");
-    match envy::from_env::<Configuration>() {
-        Ok(config) => println!("{:?}", config),
-        Err(e) => println!("Couldn't read config ({})", e),
-    };
-    let config = envy::from_env::<Configuration>().unwrap();
-    let port = config.port;
+    let port = str::parse(&std::env::var("PORT").unwrap_or("4000".to_string())).unwrap();
 
     HttpServer::new(move || {
-        App::new().wrap(middleware::Compress::default()).service(
-            web::scope("/api")
-                .app_data(Data::new(config.clone()))
-                .service(champions)
-                .service(summoner),
-        )
+        App::new()
+            .wrap(middleware::Compress::default())
+            .service(web::scope("/api").service(champions).service(summoner))
     })
     .bind(("127.0.0.1", port))?
     .run()
--- src/services/summoner.rs
+++ src/services/summoner.rs
@@ -1,119 +1,8 @@
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use std::{
-    error,
-    fmt::{self},
-};
+use crate::lib::summoner::get_summoner_ranks;
+use crate::lib::{matches, summoner::SummonerInfoResponse};
+use crate::{lib::summoner::get_summoner_info, SummonerGetDataQuery};
 
-use crate::SummonerGetDataQuery;
-
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize, Debug)]
-pub struct SummonerInfoResponse {
-    id: String,
-    accountId: String,
-    puuid: String,
-    name: String,
-    profileIconId: u32,
-    revisionDate: u64,
-    summonerLevel: u32,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct StatusBody {
-    message: String,
-    status_code: u16,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct Status {
-    status: StatusBody,
-}
-
-// struct SummonerRanks {}
-// type StringOrInteger = String | U32;
-#[allow(non_snake_case)]
-#[derive(Deserialize, Serialize, Debug)]
-struct SummonerRankInfo {
-    leagueId: String,
-    queueType: String,
-    tier: String,
-    rank: String,
-    summonerId: String,
-    summonerName: String,
-    leaguePoints: u8,
-    wins: u32,
-    losses: u32,
-    veteran: bool,
-    inactive: bool,
-    freshBlood: bool,
-    hotStreak: bool,
-}
-
-#[derive(Debug, Clone)]
-struct GetSummonerRanksError {}
-
-impl fmt::Display for GetSummonerRanksError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "invalid something broke")
-    }
-}
-
-type FetchResult<T> = std::result::Result<T, Box<dyn error::Error>>;
-
-#[derive(Deserialize, Serialize, Debug)]
-#[serde(untagged)]
-enum SummonerRanksResponse {
-    Ranks(Vec<SummonerRankInfo>),
-    Error(Status),
-}
-
-async fn get_summoner_info(
-    query: &SummonerGetDataQuery,
-    api_key: &String,
-) -> Result<SummonerInfoResponse, reqwest::Error> {
-    let url = format!(
-        "https://{0}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{1}?api_key={2}",
-        query.region, query.summoner_name, api_key
-    );
-    let response = reqwest::get(url)
-        .await?
-        .json::<SummonerInfoResponse>()
-        .await?;
-    Ok(response)
-}
-
-async fn get_summoner_ranks(
-    query: &SummonerGetDataQuery,
-    id: &String,
-    api_key: &String,
-) -> FetchResult<SummonerRanksResponse> {
-    let url = format!(
-        "https://{0}.api.riotgames.com/lol/league/v4/entries/by-summoner/{1}?api_key={2}",
-        query.region, id, api_key
-    );
-    let raw_response = reqwest::get(url).await?.text().await?;
-    let response = serde_json::from_str::<SummonerRanksResponse>(&raw_response)?;
-
-    Ok(response)
-}
-
-async fn get_summoner_matches(
-    query: &SummonerGetDataQuery,
-    puuid: &String,
-    api_key: &String,
-) -> FetchResult<Value> {
-    println!("hello");
-    let url = format!(
-        "https://{0}.api.riotgames.com/lol/match/v5/matches/by-puuid/{1}/ids?start=0&count={2}&api_key={3}",
-        query.continent, puuid, 5,api_key
-    );
-    let raw_response = reqwest::get(url).await?.text().await?;
-    let response = serde_json::from_str::<Value>(&raw_response)?;
-
-    Ok(response)
-}
-
+#[allow(unused_variables)]
 pub async fn get_summoner_data(
     query: SummonerGetDataQuery,
     api_key: &String,
@@ -132,7 +21,8 @@ pub async fn get_summoner_data(
     }
     let summoner_ranks = summoner_ranks_result.unwrap();
 
-    let summoner_matches_result = get_summoner_matches(&query, &summoner_info.puuid, api_key).await;
+    let summoner_matches_result =
+        matches::get_summoner_matches(&query, &summoner_info.puuid, api_key).await;
     if summoner_matches_result.is_err() {
         let error_response = format!("Error: {}", summoner_matches_result.unwrap_err());
         return Err(error_response);
